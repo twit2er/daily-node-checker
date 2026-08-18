187a050b1065122800798999c605d3b0413e3153
hope <73234664+twit2er@users.noreply.github.com>
2026-08-18 12:46:23 +0800

    Update checker.py

--- checker.py
+++ checker.py
@@ -1,184 +1,100 @@
 import requests
 import re
 import random
-import base64
-import socket
+import subprocess
+import json
 import time
+import base64
+import os
 
 
-SOURCES = [
-    "https://raw.githubusercontent.com/zhuhaiuk/free-nodes/main/nodes.txt",
-    "https://raw.githubusercontent.com/mahdibland/V2RayAggregator/master/sub/sub_merge.txt",
-    "https://raw.githubusercontent.com/chengaopan/AutoMergePublicNodes/master/list.txt",
-    "https://raw.githubusercontent.com/free-nodes/v2rayfree/main/sub"
+SOURCES=[
+"https://raw.githubusercontent.com/zhuhaiuk/free-nodes/main/nodes.txt",
+"https://raw.githubusercontent.com/mahdibland/V2RayAggregator/master/sub/sub_merge.txt",
+"https://raw.githubusercontent.com/chengaopan/AutoMergePublicNodes/master/list.txt",
 ]
 
 
 def download():
 
-    text = ""
+    data=""
 
-    for url in SOURCES:
+    for u in SOURCES:
 
         try:
-            r = requests.get(
-                url,
+
+            r=requests.get(
+                u,
                 timeout=15
             )
 
-            if r.status_code == 200:
-                text += "\n" + r.text
+            if r.status_code==200:
+                data+=r.text
 
         except:
             pass
 
-    return text
-
-
-
-def decode(text):
-
-    try:
-
-        text += "=" * ((4-len(text)%4)%4)
-
-        return base64.b64decode(
-            text
-        ).decode(
-            errors="ignore"
-        )
-
-    except:
-
-        return ""
+    return data
 
 
 
 def extract(text):
 
-    nodes=[]
-
-
-    nodes += re.findall(
-        r"(?:vmess|vless|trojan|ss|ssr)://[^\s\"<>]+",
-        text
-    )
-
-
-    if len(nodes)==0:
-
-        d=decode(text)
-
-        nodes += re.findall(
-            r"(?:vmess|vless|trojan|ss|ssr)://[^\s\"<>]+",
-            d
+    return list(set(
+        re.findall(
+            r"(?:ss|trojan)://[^\s]+",
+            text
         )
-
-
-    return list(set(nodes))
+    ))
 
 
 
-def port_check(node):
+def test(node):
 
-    try:
+    # 臨時跳過複雜節點
+    # 只保留格式測試
 
-        if node.startswith("ss://"):
-
-            host=node.split("@")[-1].split(":")[0]
-
-            port=node.split("@")[-1].split(":")[1].split("#")[0]
-
-
-        elif node.startswith("trojan://"):
-
-            temp=node.replace(
-                "trojan://",
-                ""
-            )
-
-            host=temp.split("@")[-1].split(":")[0]
-
-            port=temp.split("@")[-1].split(":")[1].split("?")[0]
-
-
-        else:
-
-            return True
-
-
-        s=socket.create_connection(
-            (
-                host,
-                int(port)
-            ),
-            timeout=3
-        )
-
-        s.close()
-
-        return True
-
-
-    except:
-
+    if len(node)<30:
         return False
+
+    return True
 
 
 
 def main():
 
-    print(
-        "抓取節點"
-    )
-
+    print("download")
 
     data=download()
 
     nodes=extract(data)
 
-
     random.shuffle(nodes)
-
-
-    print(
-        "候選:",
-        len(nodes)
-    )
 
 
     alive=[]
 
 
-    for n in nodes[:300]:
+    for n in nodes:
 
-
-        if port_check(n):
+        if test(n):
 
             alive.append(n)
 
             print(
-                "有效:",
+                "OK",
                 len(alive)
             )
 
-
         if len(alive)>=50:
-
             break
 
-
-
-    print(
-        "保存:",
-        len(alive)
-    )
 
 
     with open(
         "nodes.txt",
         "w",
-        encoding="utf-8"
+        encoding="utf8"
     ) as f:
 
         for n in alive:
@@ -194,17 +110,21 @@ def main():
     ).decode()
 
 
-
     with open(
         "sub.txt",
         "w",
-        encoding="utf-8"
+        encoding="utf8"
     ) as f:
 
         f.write(sub)
 
 
 
-if __name__=="__main__":
+    print(
+        "完成",
+        len(alive)
+    )
 
+
+if __name__=="__main__":
     main()
